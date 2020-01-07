b0441fcaf83d9f6b2b7e394705fba03052a6aac5
nicole <nicolelihui@outlook.com>
2020-01-07 10:26:23 +0800

    Local Add Resolve #1

---
+++ src/components/LocalAdd.vue
@@ -0,0 +1,86 @@
+<template>
+  <div
+    class="text-center"
+  >
+    <v-card>
+      <v-toolbar
+        color="indigo"
+        dark
+      >
+        <v-app-bar-nav-icom></v-app-bar-nav-icom>
+        <v-toolbar-title>Add Function</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+      </v-toolbar>
+      
+      <v-form v-model="addfrom">
+        <v-cantainer>
+          <v-row justify="center">
+            <v-col 
+              cols="12"
+              md="3"  
+            >
+              <v-text-field
+                v-model="number1"
+                label="Number 1"
+              ></v-text-field>
+            </v-col>
+
+            <v-col 
+              cols="12"
+              md="1"  
+            >
+              <span>+</span>
+            </v-col>
+
+            <v-col 
+              cols="12"
+              md="3"  
+            >
+              <v-text-field
+                v-model="number2"
+                label="Number 2"
+              ></v-text-field>
+            </v-col>
+
+            <v-col 
+              cols="12"
+              md="1"  
+            >
+              <span>=</span>
+            </v-col>
+
+            <v-col 
+              cols="12"
+              md="3"  
+            >
+              <v-text-field
+                v-model="result"
+                label="Result"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-cantainer>
+      </v-form>
+    </v-card>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'LocalAdd',
+  
+  data: () => ({
+    number1: 0,
+    number2: 0,
+  }),
+  computed: {
+    result: function () {
+      return Number(this.number1) + Number(this.number2)
+    }
+  },
+})
+</script>
